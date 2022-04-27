e18f0c41762fa29ee9ea3ab0582ca59db54a7ed6
Rock Storm <rockstorm@gmx.com>
2022-04-27 17:41:30 +0100

    Add option to set git user UID

--- Dockerfile
+++ Dockerfile
@@ -14,17 +14,18 @@ RUN ssh-keygen -A
 # Define variables
 ENV GIT_USER git
 ENV GIT_GROUP git
-ENV HOME_GIT_USER /home/${GIT_USER}
-ENV GIT_REPOSITORIES_PATH /srv/git
-ENV SSH_AUTHORIZED_KEYS_PATH ${HOME_GIT_USER}/.ssh
+ENV GIT_HOME /home/${GIT_USER}
+ENV SSH_AUTHORIZED_KEYS_PATH ${GIT_HOME}/.ssh
 ENV SSH_AUTHORIZED_KEYS_FILE ${SSH_AUTHORIZED_KEYS_PATH}/authorized_keys
+ENV GIT_SHELL /usr/bin/git-shell
+ENV GIT_REPOSITORIES_PATH /srv/git
 ENV SETUP_FILE /sbin/setup
 
 # Create the git user and enable login by assigning a simple password
 # Note that BusyBox implementation of `adduser` differs from Debian's
 # and therefore options behave slightly differently
 RUN set -eux; \
-  adduser --disabled-password --shell /usr/bin/git-shell "${GIT_USER}"; \
+  adduser --disabled-password --shell "${GIT_SHELL}" "${GIT_USER}"; \
   echo "${GIT_USER}:12345" | chpasswd
 
 # Create the folder(s) to hold the authorized_keys file
@@ -32,7 +33,7 @@ RUN mkdir -p ${SSH_AUTHORIZED_KEYS_PATH}
 
 # Restrict git user to git commands
 # See `man git-shell`
-COPY git-shell-commands /home/${GIT_USER}/git-shell-commands
+COPY git-shell-commands ${GIT_HOME}/git-shell-commands
 
 COPY setup.sh ${SETUP_FILE}
 
--- setup.sh
+++ setup.sh
@@ -3,18 +3,30 @@ set -e
 
 if [ -n "${DEBUG}" ]; then set -x; fi
 
-# Throw a warning if authorized_keys is not found
-if [ ! -f "${SSH_AUTHORIZED_KEYS_FILE}" ]; then
-    echo "File '${SSH_AUTHORIZED_KEYS_FILE}' not found.";
-    echo "No user will be able to log in using a public key."
+# Set specific UID and GID for the git user
+if [ -n "${GIT_USER_UID}" ] && \
+	   [ "${GIT_USER_UID}" != "$(id -u "${GIT_USER}")" ] && \
+	   [ "${GIT_USER_UID}" != 0 ]; then
+	if [ -z "${GIT_USER_GID}" ]; then
+		GIT_USER_GID="${GIT_USER_UID}";
+	fi
+	# Due to no `usermod` on Alpine Linux, we need to delete and
+	# re-add the git user
+	# `deluser` deletes both the user and the group
+	deluser "${GIT_USER}"
+	addgroup -g "${GIT_USER_GID}" "${GIT_GROUP}"
+	adduser \
+		--gecos 'Linux User' \
+		--shell "${GIT_SHELL}" \
+		--uid "${GIT_USER_UID}" \
+		--ingroup "${GIT_GROUP}" \
+		--no-create-home \
+		--disabled-password "${GIT_USER}"
+	echo "${GIT_USER}:12345" | chpasswd
 fi
 
-# Change password of the git user using an environment variable
-if [ -n "${GIT_PASSWORD}" ]; then
-    echo "${GIT_USER}":"${GIT_PASSWORD}" | chpasswd
-fi
-
-# Change password of the git user using a file
+# Change password of the git user
+# A password on file is preferred over the environment variable one
 if [ -n "${GIT_PASSWORD_FILE}" ]; then
     if [ -f "${GIT_PASSWORD_FILE}" ]; then
 		echo "${GIT_USER}:$(cat "${GIT_PASSWORD_FILE}")" | chpasswd
@@ -22,14 +34,21 @@ if [ -n "${GIT_PASSWORD_FILE}" ]; then
         echo "File '${GIT_PASSWORD_FILE}' not found."
         echo "Password for ${GIT_USER} is unchanged."
     fi
+elif [ -n "${GIT_PASSWORD}" ]; then
+    echo "${GIT_USER}":"${GIT_PASSWORD}" | chpasswd
 fi
 
 # Make the git user the onwer of all repositories
-export GIT_REPOSITORIES_PATH
 if [ -d "${GIT_REPOSITORIES_PATH}" ]; then
     chown -R "${GIT_USER}":"${GIT_GROUP}" "${GIT_REPOSITORIES_PATH}"/.
 else
     echo "Directory '${GIT_REPOSITORIES_PATH}' not found."
+fi
+
+# Throw a warning if authorized_keys is not found
+if [ ! -f "${SSH_AUTHORIZED_KEYS_FILE}" ]; then
+    echo "File '${SSH_AUTHORIZED_KEYS_FILE}' not found.";
+    echo "No user will be able to log in using a public key."
 fi
 
 # Replace host SSH keys (if given)
@@ -51,7 +70,7 @@ fi
 #     git clone [user@]host.xz:/srv/git/repo.git
 if [ -n "${REPOSITORIES_HOME_LINK}" ]; then
     if [ -d "${REPOSITORIES_HOME_LINK}" ]; then
-        ln -sf "${REPOSITORIES_HOME_LINK}" "${HOME_GIT_USER}"
+        ln -sf "${REPOSITORIES_HOME_LINK}" "${GIT_HOME}"
     else
         echo "Directory '${REPOSITORIES_HOME_LINK}' not found."
         echo "Home link not created."
