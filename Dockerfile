9e4b19341fa6a1d75eed69644ab6942decd9bfcd
Rock Storm <rockstorm@gmx.com>
2022-04-28 17:41:19 +0100

    Add admin commands to git user and cleanup tabs

--- Dockerfile
+++ Dockerfile
@@ -2,38 +2,42 @@ FROM alpine:3.15
 
 ARG EXTRA_PACKAGES=""
 RUN set -eux; \
-  apk add --no-cache \
-    git=2.34.2-r0 \
-    openssh=8.8_p1-r1 \
-    $EXTRA_PACKAGES \
-  ;
+    apk add --no-cache \
+        git=2.34.2-r0 \
+        openssh=8.8_p1-r1 \
+        $EXTRA_PACKAGES \
+    ;
 
 # Generate SSH host keys
 RUN ssh-keygen -A
 
 # Define variables
-ENV GIT_USER git
-ENV GIT_GROUP git
-ENV GIT_HOME /home/${GIT_USER}
-ENV SSH_AUTHORIZED_KEYS_PATH ${GIT_HOME}/.ssh
-ENV SSH_AUTHORIZED_KEYS_FILE ${SSH_AUTHORIZED_KEYS_PATH}/authorized_keys
-ENV GIT_SHELL /usr/bin/git-shell
-ENV GIT_REPOSITORIES_PATH /srv/git
-ENV SETUP_FILE /sbin/setup
+ENV GIT_USER=git \
+    GIT_GROUP=git
+ENV GIT_HOME=/home/${GIT_USER} \
+    SSH_AUTHORIZED_KEYS_FILE=${GIT_HOME}/.ssh/authorized_keys \
+    GIT_REPOSITORIES_PATH=/srv/git \
+    SETUP_FILE=/sbin/setup
 
 # Create the git user and enable login by assigning a simple password
 # Note that BusyBox implementation of `adduser` differs from Debian's
 # and therefore options behave slightly differently
 RUN set -eux; \
-  adduser --disabled-password --shell "${GIT_SHELL}" "${GIT_USER}"; \
-  echo "${GIT_USER}:12345" | chpasswd
-
-# Create the folder(s) to hold the authorized_keys file
-RUN mkdir -p ${SSH_AUTHORIZED_KEYS_PATH}
+    adduser --disabled-password --shell "$(which git-shell)" "${GIT_USER}"; \
+    echo "${GIT_USER}:12345" | chpasswd
 
 # Restrict git user to git commands
-# See `man git-shell`
+# See `git-shell(1)`
 COPY git-shell-commands ${GIT_HOME}/git-shell-commands
+RUN set -eux; \
+    cd ${GIT_HOME}/git-shell-commands; \
+    cmds="ls mkdir rm vi"; \
+    for c in $cmds; do \
+        ln -s $(which $c) .; \
+    done;
+
+# Delete Alpine welcome message
+RUN rm /etc/motd
 
 COPY setup.sh ${SETUP_FILE}
 
--- setup.sh
+++ setup.sh
@@ -5,31 +5,31 @@ if [ -n "${DEBUG}" ]; then set -x; fi
 
 # Set specific UID and GID for the git user
 if [ -n "${GIT_USER_UID}" ] && \
-	   [ "${GIT_USER_UID}" != "$(id -u "${GIT_USER}")" ] && \
-	   [ "${GIT_USER_UID}" != 0 ]; then
-	if [ -z "${GIT_USER_GID}" ]; then
-		GIT_USER_GID="${GIT_USER_UID}";
-	fi
-	# Due to no `usermod` on Alpine Linux, we need to delete and
-	# re-add the git user
-	# `deluser` deletes both the user and the group
-	deluser "${GIT_USER}"
-	addgroup -g "${GIT_USER_GID}" "${GIT_GROUP}"
-	adduser \
-		--gecos 'Linux User' \
-		--shell "${GIT_SHELL}" \
-		--uid "${GIT_USER_UID}" \
-		--ingroup "${GIT_GROUP}" \
-		--no-create-home \
-		--disabled-password "${GIT_USER}"
-	echo "${GIT_USER}:12345" | chpasswd
+       [ "${GIT_USER_UID}" != "$(id -u "${GIT_USER}")" ] && \
+       [ "${GIT_USER_UID}" != 0 ]; then
+    if [ -z "${GIT_USER_GID}" ]; then
+        GIT_USER_GID="${GIT_USER_UID}";
+    fi
+    # Due to no `usermod` on Alpine Linux, we need to delete and
+    # re-add the git user
+    # `deluser` deletes both the user and the group
+    deluser "${GIT_USER}"
+    addgroup -g "${GIT_USER_GID}" "${GIT_GROUP}"
+    adduser \
+        --gecos 'Linux User' \
+        --shell "${GIT_SHELL}" \
+        --uid "${GIT_USER_UID}" \
+        --ingroup "${GIT_GROUP}" \
+        --no-create-home \
+        --disabled-password "${GIT_USER}"
+    echo "${GIT_USER}:12345" | chpasswd
 fi
 
 # Change password of the git user
 # A password on file is preferred over the environment variable one
 if [ -n "${GIT_PASSWORD_FILE}" ]; then
     if [ -f "${GIT_PASSWORD_FILE}" ]; then
-		echo "${GIT_USER}:$(cat "${GIT_PASSWORD_FILE}")" | chpasswd
+        echo "${GIT_USER}:$(cat "${GIT_PASSWORD_FILE}")" | chpasswd
     else
         echo "File '${GIT_PASSWORD_FILE}' not found."
         echo "Password for ${GIT_USER} is unchanged."
@@ -38,9 +38,13 @@ elif [ -n "${GIT_PASSWORD}" ]; then
     echo "${GIT_USER}":"${GIT_PASSWORD}" | chpasswd
 fi
 
-# Make the git user the onwer of all repositories
+# Make the git user the onwer of all repositories and (re)set file
+# permissions
 if [ -d "${GIT_REPOSITORIES_PATH}" ]; then
-    chown -R "${GIT_USER}":"${GIT_GROUP}" "${GIT_REPOSITORIES_PATH}"/.
+    cd "${GIT_REPOSITORIES_PATH}"/.
+    chown -R "${GIT_USER}":"${GIT_GROUP}" .
+    find . -type f -exec chmod u=rwX,go=rX '{}' \;
+    find . -type d -exec chmod u=rwx,go=rx '{}' \;
 else
     echo "Directory '${GIT_REPOSITORIES_PATH}' not found."
 fi
@@ -63,11 +67,7 @@ if [ -n "${SSH_HOST_KEYS_PATH}" ]; then
     fi
 fi
 
-# Link the repositories folder on git user's home directory to access
-# repos with:
-#     git clone [user@]host.xz:repo.git
-# instead of:
-#     git clone [user@]host.xz:/srv/git/repo.git
+# Link the repositories folder on git user's home directory
 if [ -n "${REPOSITORIES_HOME_LINK}" ]; then
     if [ -d "${REPOSITORIES_HOME_LINK}" ]; then
         ln -sf "${REPOSITORIES_HOME_LINK}" "${GIT_HOME}"
@@ -80,15 +80,14 @@ fi
 # Add the git user to the group with write access to the docker socket
 docker_socket_path="/var/run/docker.sock"
 if [ -S "${docker_socket_path}" ]; then
-	docker_group="$(stat -c '%G' ${docker_socket_path})"
-	docker_group_id="$(stat -c '%g' ${docker_socket_path})"
-	if [ "${docker_group}" = "UNKNOWN" ]; then
-		docker_group="docker"
-		addgroup -g "${docker_group_id}" "${docker_group}"
-	fi
-	addgroup "${GIT_USER}" "${docker_group}"
+    docker_group="$(stat -c '%G' ${docker_socket_path})"
+    docker_group_id="$(stat -c '%g' ${docker_socket_path})"
+    if [ "${docker_group}" = "UNKNOWN" ]; then
+        docker_group="docker"
+        addgroup -g "${docker_group_id}" "${docker_group}"
+    fi
+    addgroup "${GIT_USER}" "${docker_group}"
 fi
-
 
 # Start the ssh server
 /usr/sbin/sshd -D
