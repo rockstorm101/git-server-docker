3cd3ddaf244f498dab6f60411dd9d07e9e5e6e56
Rock Storm <rockstorm@gmx.com>
2024-01-07 12:24:01 +0100

    style: Untabify setup code

--- 10-setup.sh
+++ 10-setup.sh
@@ -79,8 +79,8 @@ fi
 # Configure the SSH server configuration file
 SSHD_CONFIG_FILE='/etc/ssh/sshd_config'
 if [ -n "${SSH_AUTH_METHODS-}" ]; then
-	sed -i "s/.*AuthenticationMethods.*//g" ${SSHD_CONFIG_FILE}
-	echo "AuthenticationMethods ${SSH_AUTH_METHODS}" >> ${SSHD_CONFIG_FILE}
+    sed -i "s/.*AuthenticationMethods.*//g" ${SSHD_CONFIG_FILE}
+    echo "AuthenticationMethods ${SSH_AUTH_METHODS}" >> ${SSHD_CONFIG_FILE}
 fi
 
 # Link the repositories folder on git user's home directory
